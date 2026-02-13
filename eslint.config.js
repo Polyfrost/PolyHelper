e8f0a2740175f66a1d74a92ea2a6fa4d428d8f74
RuiNtD <RuiNtD@outlook.com>
2026-02-12 19:02:26 -0700

    Migrate ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 // @ts-check
 
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 import prettier from "eslint-config-prettier/flat";
 
-export default ts.config(
+export default defineConfig(
   { languageOptions: { globals: globals.node } },
   js.configs.recommended,
   ...ts.configs.recommendedTypeChecked,
