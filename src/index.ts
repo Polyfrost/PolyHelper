db53a30bd5b30b6a549755a9a7c0584f2a35bb7f
RuiNtD <RuiNtD@outlook.com>
2026-03-28 23:50:16 -0600

    More consola

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
-import { SapphireClient } from "@sapphire/framework";
+import { SapphireClient, LogLevel, type ILogger } from "@sapphire/framework";
 import { GatewayIntentBits, Partials } from "discord.js";
 import "@sapphire/plugin-editable-commands/register";
 import { setup } from "@skyra/env-utilities";
 import consola from "consola";
 
 setup();
+
+const consolaLogger: ILogger = {
+  has: (level) => level >= LogLevel.Debug,
+  trace: (...args: any[]) => consola.trace.raw(...args),
+  debug: (...args: any[]) => consola.debug.raw(...args),
+  info: (...args: any[]) => consola.info.raw(...args),
+  warn: (...args: any[]) => consola.warn.raw(...args),
+  error: (...args: any[]) => consola.error.raw(...args),
+  fatal: (...args: any[]) => consola.fatal.raw(...args),
+  write: (level, ...args: any[]) => {
+    switch (level) {
+      case LogLevel.Trace:
+        return consola.trace.raw(...args);
+      case LogLevel.Debug:
+        return consola.debug.raw(...args);
+      case LogLevel.Info:
+        return consola.info.raw(...args);
+      case LogLevel.Warn:
+        return consola.warn.raw(...args);
+      case LogLevel.Error:
+        return consola.error.raw(...args);
+      case LogLevel.Fatal:
+        return consola.fatal.raw(...args);
+      default:
+        return consola.log.raw(...args);
+    }
+  },
+};
 
 const client = new SapphireClient({
   intents: [
@@ -16,6 +44,7 @@ const client = new SapphireClient({
     GatewayIntentBits.MessageContent,
   ],
   partials: [Partials.Channel],
+  logger: { instance: consolaLogger },
   loadMessageCommandListeners: true,
 });
 
